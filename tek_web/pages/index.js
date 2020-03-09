951b0ba40421e2410e212818c8a44ed7610d5920
quangdx <quangdo2206@gmail.com>
2020-03-09 17:57:15 +0700

    create project

---
+++ tek_web/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Index extends Component {
+
+	render() {
+		return (
+            <div>
+                This is homepage
+            </div>
+        )
+	}
+}
+
+export default Index;
